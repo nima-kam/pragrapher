3244829f64cd732466f197a9ac22a43632002fcd
Nima <56191102+nima-kam@users.noreply.github.com>
2021-10-18 18:46:52 +0330

    userModel added

--- Back-end/app.py
+++ Back-end/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 from resources.login import *
+from setting import *
 
-app = Flask(__name__)
+app = create_app(__name__)
 api = Api(app=app)
 
-api.add_resource(Login, "/authenticate")
+api.add_resource(Login, "/auth")
 api.add_resource(Register, "/register")
 
 if __name__ == '__main__':
