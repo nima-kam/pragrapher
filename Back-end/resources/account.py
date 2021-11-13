93525fb25fd4ebd1960a0b8ed89a27ba8f24fae9
Nima <56191102+nima-kam@users.noreply.github.com>
2021-11-13 12:35:39 +0330

    user pp get added

--- Back-end/resources/account.py
+++ Back-end/resources/account.py
@@ -1,6 +1,7 @@
 import os
 from flask_restful import Resource
 from flask import request, redirect, make_response, url_for, jsonify, render_template
+from http import HTTPStatus as hs
 import jwt
 from sqlalchemy.orm.base import NOT_EXTENSION
 from config import jwt_secret_key
@@ -8,7 +9,8 @@ from tools.db_tool import engine
 from tools.image_tool import get_extension
 from tools.token_tool import authorize
 
-from db_models.users import change_user_image, edit_bio, get_one_user, add_user, change_pass, edit_fname, edit_dob
+from db_models.users import change_user_image, edit_bio, get_one_user, add_user, change_pass, edit_fname, edit_dob, \
+    UserModel
 from tools.string_tools import gettext
 from typing import Union, Dict
 
@@ -38,9 +40,11 @@ class myprofile(Resource):
         req_data = request.json
         v = is_available(req_data, "profile_name")
         if not v[0]:
-            return make_response( jsonify(v[1], 401))
+            return make_response(jsonify(v[1], 401))
         edit_fname(current_user, req_data['profile_name'], self.engine)
-        return make_response(jsonify(message=gettext("item_edited").format("profile name")) ) # redirect(url_for("myprofile"))
+        return make_response(
+            jsonify(message=gettext("item_edited").format("profile name")))  # redirect(url_for("myprofile"))
+
 
 
 class bio(Resource):
@@ -55,7 +59,7 @@ class bio(Resource):
         if bio_data is None:
             return make_response(jsonify(message=gettext("item_not_found").format("bio")), 401)
         edit_bio(current_user, req_data['bio'], self.engine)
-        return make_response(jsonify(message=gettext("item_edited").format("bio")),200)
+        return make_response(jsonify(message=gettext("item_edited").format("bio")), 200)
         # return redirect(url_for("myprofile"))
 
 
@@ -71,7 +75,7 @@ class dob(Resource):
         if bio_data is None:
             return make_response(jsonify(message=gettext("item_not_found").format("dob")), 401)
         edit_dob(current_user, req_data['dob'], self.engine)
-        return make_response(jsonify(message=gettext("item_edited").format("dob")),200)
+        return make_response(jsonify(message=gettext("item_edited").format("dob")), 200)
         # return redirect(url_for("myprofile"))
 
 
@@ -80,12 +84,16 @@ class profile_picture(Resource):
         self.engine = kwargs['engine']
 
     @authorize
+    def get(self, current_user: UserModel):
+        return make_response(jsonify(url=current_user.image), hs.OK)
+
+    @authorize
     def post(self, current_user):
         """insert or change current user profile picture"""
         files = request.files
         file = files.get('file')
         if 'file' not in request.files:
-            return make_response( jsonify(message=gettext("upload_no_file")), 400)
+            return make_response(jsonify(message=gettext("upload_no_file")), 400)
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
@@ -102,7 +110,7 @@ class profile_picture(Resource):
                 pass
             file.save(os.getcwd() + url)
             change_user_image(current_user, url, self.engine)
-            return make_response(jsonify(message=gettext("upload_success")),200)
+            return make_response(jsonify(message=gettext("upload_success")), 200)
 
 
 class password(Resource):
@@ -116,7 +124,7 @@ class password(Resource):
         res = change_pass(current_user, req_data['old_password'], req_data['new_password'], self.engine)
 
         if res:
-            return make_response(jsonify(message=gettext("item_edited").format("password")),200)
+            return make_response(jsonify(message=gettext("item_edited").format("password")), 200)
 
         else:
             return make_response(jsonify(message=gettext("wrong_pass")), 403)
