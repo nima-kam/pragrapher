eae558f55ed60de5d169e8383f31b02f51d114a2
Nima <56191102+nima-kam@users.noreply.github.com>
2021-12-04 14:25:38 +0330

    book edit and delete added

--- Back-end/db_models/book.py
+++ Back-end/db_models/book.py
@@ -5,25 +5,27 @@ from typing import List
 
 import datetime
 
+
 class book_model(Base):
     __tablename__ = "book"
     id = db.Column(db.VARCHAR(250), primary_key=True)
     name = db.Column(db.VARCHAR(250), nullable=False)
     genre = db.Column(db.VARCHAR(100), nullable=False)
-    reserved = db.Column(db.Boolean,default=False, nullable=False)
+    reserved = db.Column(db.Boolean, default=False, nullable=False)
     reserved_time = db.Column(db.DATETIME, nullable=True)
     reserved_by = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=True)
 
+    price = db.Column(db.Integer, nullable=False)
     author = db.Column(db.VARCHAR(200), nullable=False)
     modified_time = db.Column(db.DATETIME, nullable=False)
     description = db.Column(db.VARCHAR(250), nullable=True)
     seller_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
-    community_id = db.Column(db.VARCHAR(
-        30), db.ForeignKey("community.id"), nullable=False)
+    community_id = db.Column(db.VARCHAR(30),
+                             db.ForeignKey("community.id"), nullable=False)
     community_name = db.Column(db.VARCHAR(200), nullable=False)
     image = db.Column(db.VARCHAR(250), nullable=True)
 
-    def __init__(self, name, genre, author, community_id, community_name, description, seller_id ):
+    def __init__(self, name, genre, author, community_id, community_name, description, price, seller_id):
         self.id = community_id + "," + datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
         self.name = name
         self.genre = genre
@@ -33,14 +35,15 @@ class book_model(Base):
         self.modified_time = datetime.datetime.now()
         self.description = description
         self.seller_id = seller_id
+        self.price = price
 
     @property
     def json(self):
-
         dic = {"id": self.id,
                "name": self.name,
                "genre": self.genre,
                "author": self.author,
+               "price": self.price,
                "modified_time": str(self.modified_time),
                "reserved_time": str(self.reserved_time),
                "description": self.description,
@@ -52,10 +55,49 @@ class book_model(Base):
         return dic
 
 
-def add_book(name, genre, author, community_id, community_name, description, seller_id , engine):
+def add_book(name, genre, author, community_id, community_name, description, price, seller_id, engine):
     session = make_session(engine)
-    jwk_user = book_model(name=name, genre=genre, description=description, seller_id=seller_id, community_id=community_id,
-                               community_name=community_name,author=author)
+    jwk_user = book_model(name=name, genre=genre, description=description, seller_id=seller_id,
+                          community_id=community_id,
+                          community_name=community_name, price=price, author=author)
     session.add(jwk_user)
     session.commit()
     return jwk_user.json
+
+
+def get_one_book(book_id, community_name, engine):
+    session = make_session(engine)
+    book: book_model = session.query(book_model).filter(
+        db.and_(book_model.id == book_id, book_model.community_name == community_name)).first()
+    return book
+
+
+def edit_book(book: book_model, name, genre, author, price, description, engine):
+    session = make_session(engine)
+    b: book_model = session.query(book_model).filter(
+        db.and_(book_model.id == book.id)).first()
+
+    if name is not None:
+        b.name = name
+    if genre is not None:
+        b.genre = genre
+    if author is not None:
+        b.author = author
+    if description is not None:
+        b.description = description
+    if price is not None:
+        b.price = price
+    session.flush()
+    session.commit()
+    return b.json
+
+
+def delete_book(book: book_model, engine):
+    session = make_session(engine)
+    b: book_model = session.query(book_model).filter(
+        db.and_(book_model.id == book.id)).first()
+
+    session.delete(b)
+
+    session.flush()
+    session.commit()
--- Back-end/resources/book.py
+++ Back-end/resources/book.py
@@ -2,7 +2,7 @@ import os
 from http import HTTPStatus as hs
 from flask_restful import Resource, reqparse
 from flask import request, make_response, jsonify
-from db_models.book import add_book
+from db_models.book import add_book, get_one_book, edit_book, book_model, delete_book
 from db_models.paragraph import add_paragraph, add_reply, delete_paragraph, get_impression, get_one_paragraph, \
     paragraph_model, edit_paragraph
 from db_models.users import UserModel, add_notification
@@ -14,14 +14,17 @@ from db_models.community import get_community, get_role, add_notification_to_sub
 from db_models.paragraph import change_impression
 from tools.string_tools import gettext
 
+
 class book(Resource):
+    """add a book for sell and edit or delete it"""
+
     def __init__(self, **kwargs):
         self.engine = kwargs['engine']
 
     @authorize
     def post(self, current_user: UserModel, c_name):
         req_data = request.json
-#name, genre, author, community_id, community_name, description, seller_id
+        # name, genre, author, community_id, community_name, description, seller_id
         try:
             print(req_data['name'])
         except:
@@ -42,17 +45,86 @@ class book(Resource):
         except:
             msg = gettext("book_item_needed").format("description")
             return {'message': msg}, hs.BAD_REQUEST
+        try:
+            print(req_data['price'])
+        except:
+            msg = gettext("book_item_needed").format("price")
+            return {'message': msg}, hs.BAD_REQUEST
 
         # check if community name not repeated **
         comu = get_community(c_name, self.engine)
         if comu is None:
             return make_response(jsonify(message=gettext("community_not_found")), 401)
 
-        role = get_role(current_user.id, comu.id, self.engine) 
+        role = get_role(current_user.id, comu.id, self.engine)
         if role == -1:
             return make_response(jsonify(message=gettext("permission_denied")), 403)
-        cm = add_book(req_data['name'], req_data['genre'], req_data['author']  ,comu.id, comu.name,req_data['description'],current_user.id,
-                           self.engine)
+        cm = add_book(req_data['name'], req_data['genre'], req_data['author'], comu.id, comu.name,
+                      req_data['description'], price=req_data['price'], seller_id=current_user.id,
+                      engine=self.engine)
         print(cm, "\n\n\n\n\n\n\n")
-        add_notification(current_user.id, current_user.email , "کتاب{}به فروشگاه اضافه شد".format(req_data['name']),"کتاب اضافه شد", self.engine)
+        add_notification(current_user.id, current_user.email, "کتاب{}به فروشگاه اضافه شد".format(req_data['name']),
+                         "کتاب جدید اضافه شد", self.engine)
         return make_response(jsonify(message=gettext("book_add_success"), res=cm), 200)
+
+    @authorize
+    def put(self, current_user: UserModel, c_name):
+        req_data = request.json
+        try:
+            b_id = req_data["book_id"]
+        except:
+            msg = gettext("book_item_needed").format("book_id")
+            return {'message': msg}, hs.BAD_REQUEST
+        try:
+            b_name = req_data.get('name', None)
+            author = req_data.get('author', None)
+            desc = req_data.get('description', None)
+            genre = req_data.get('genre', None)
+            price = req_data.get('price', None)
+        except:
+            msg = gettext("book_item_needed").format("items")
+            return {'message': msg}, hs.BAD_REQUEST
+        #
+        # comu = get_community(c_name, self.engine)
+        # if comu is None:
+        #     return make_response(jsonify(message=gettext("community_not_found")), 401)
+        #
+        # role = get_role(current_user.id, comu.id, self.engine)
+        # if role == -1:
+        #     return make_response(jsonify(message=gettext("permission_denied")), 403)
+
+        cbook: book_model = get_one_book(book_id=b_id, community_name=c_name, engine=self.engine)
+
+        if cbook.seller_id == current_user.id:
+            book_js = edit_book(cbook, b_name, genre, author,price=price, description=desc, engine=self.engine)
+            msg = gettext("book_edit_success")
+            return {"message": msg, "book": book_js}, hs.OK
+        else:
+            return make_response(jsonify(message=gettext("permission_denied")), 403)
+
+    @authorize
+    def delete(self, current_user, c_name):
+        req_data = request.json
+        try:
+            b_id = req_data["book_id"]
+        except:
+            msg = gettext("book_item_needed").format("book_id")
+            return {'message': msg}, hs.BAD_REQUEST
+
+        comu = get_community(c_name, self.engine)
+        if comu is None:
+            return make_response(jsonify(message=gettext("community_not_found")), 401)
+
+        role = get_role(current_user.id, comu.id, self.engine)
+        if role == -1:
+            return make_response(jsonify(message=gettext("permission_denied")), 403)
+
+        else:
+            cbook = get_one_book(book_id=b_id, community_name=c_name, engine=engine)
+
+            if role == 1 or current_user.id == cbook.seller_id:
+                delete_book(cbook)
+                msg = gettext("store_item_delete").format("Book")
+                return {"message": msg}, hs.OK
+            else:
+                return make_response(jsonify(message=gettext("permission_denied")), 403)
